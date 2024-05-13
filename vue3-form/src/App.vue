eec71126c4d48ab0464f62328ea4ece6b16f2414
jinwoo park <swat018@gmail.com>
2024-05-13 13:31:15 +0900

    feat : Vue.js 최종 프로젝트 - 프로젝트 생성 및 로그인 폼 UI 구성

---
+++ vue3-form/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <form action="">
+    <div>
+      <label for="userId">ID:</label>
+      <input id="userID" type="text" v-model="username">
+    </div>
+    <div>
+      <label for="password">PW:</label>
+      <input type="text" v-model="password">
+    </div>
+    <button type="submit">로그인</button>
+  </form>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        username: '',
+        password: ''
+      }
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
