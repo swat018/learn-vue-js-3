e3733b74c44b30463b42e24e2a2ad49dfc7c3195
jinwoo park <swat018@gmail.com>
2024-05-13 11:55:03 +0900

    feat : Vue 싱글 파일 컴포넌트 - Vue 컴포넌트 등록 방법 및 명명 규칙

--- vue3-cli/src/App.vue
+++ vue3-cli/src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <AppHeader></AppHeader>
   <div>
     {{ message }}
   </div>
@@ -6,18 +7,28 @@
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      message: 'hi'
-    }
-  },
-  methods: {
-    showAlert() {
-      alert('hello')
+  // import 컴포넌트이름 from './컴포넌트 경로'
+  import AppHeader from "./components/AppHeader.vue";
+
+  export default {
+    components: {
+      // '컴포넌트이름': 컴포넌트이름
+      // 'app-header': AppHeader,
+      // 'AppHeader': AppHeader
+      // AppHeader: AppHeader
+      AppHeader
+    },
+    data() {
+      return {
+        message: 'hi'
+      }
+    },
+    methods: {
+      showAlert() {
+        alert('hello')
+      }
     }
   }
-}
 </script>
 
 <style scoped>
---
+++ vue3-cli/src/components/AppHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>App Header</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
